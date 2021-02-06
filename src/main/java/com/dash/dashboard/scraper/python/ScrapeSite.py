9153291ea73ffb2591433102138f78d894aca018
Brandon Pahla <brandon.m.paahla@gmail.com>
2021-02-06 19:00:25 +0200

    added a class that opens any site

---
+++ src/main/java/com/dash/dashboard/scraper/python/ScrapeSite.py
@@ -0,0 +1,54 @@
+"""
+Scrape data(soup) from returned data from loginScape
+and return Assignment data
+"""
+
+from loginScrape import Scrape
+from bs4 import BeautifulSoup
+
+class Site:
+    def __init__(self, scraper):
+        self.soup , self.session = scraper.login()
+        self.sites = []
+    
+    def get_sites(self):
+        titles = self.soup.findAll("span", {"class" : "fullTitle"})
+        for title in titles:
+            self.sites.append(title.string)
+        return self.sites
+
+    def site_in_sites(self, site_name):
+        sites = self.get_sites()
+        for site in sites:
+            if site == site_name:
+                return True
+        return False
+    
+    def get_site_link(self, site_name):
+        if self.site_in_sites(site_name):
+            try:
+                attributes = {"title": site_name}
+                link = self.soup.findAll("a", attributes)
+                return link[0]["href"] 
+            except:
+                print("there was an exception")
+
+    def go_to_site(self, site_name):
+        link = self.get_site_link(site_name)
+        try:
+            site = self.session.get(link)
+            soup = BeautifulSoup(site.text, "html.parser")
+            return soup, self.session
+        except:
+            pass
+
+
+s = Scrape("phlbra004", "icui4cubenarsky")
+
+sites = Site(s)
+
+soup, session = sites.go_to_site('BIO1004F,2020')
+
+print(soup.prettify())
+
+
