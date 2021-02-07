ad1e01f78152ee199b786b316705f99154fcb722
Brandon Pahla <brandon.m.paahla@gmail.com>
2021-02-07 21:03:21 +0200

    Added a classes to scrape for announcements and Tests and Quizzåes

--- src/main/java/com/dash/dashboard/scraper/python/tools.py
+++ src/main/java/com/dash/dashboard/scraper/python/tools.py
@@ -4,8 +4,7 @@ class Announcements:
         self.soup , self.session = announcements_soup
         self.announcements_list = []
 
-    
-    def get_all_announcements(self):
+    def get_all(self):
         self.get_announcements()
         return self.announcements_list[:len(self.announcements_list)-1]
 
@@ -13,7 +12,10 @@ class Announcements:
         print(self.soup.prettify())
 
     def get_table(self):
-        table = self.soup.find("table", {"class":"table table-hover table-striped table-bordered"})
+        try:
+            table = self.soup.find("table", {"class":"table table-hover table-striped table-bordered"})
+        except:
+            print("There was an exception")
         return table
     
     def get_announcements(self):
@@ -38,3 +40,38 @@ class Announcements:
             except:
                 pass
             self.announcements_list.append(days_announcements)
+
+class Tests:
+    def __init__(self, tests_soup):
+        self.soup, self.session = tests_soup
+        self.tests_list = []
+
+    def get_all(self):
+        self.get_tests()
+        return self.tests_list
+
+    def print_soup(self):
+        print(self.soup.prettify())
+
+    def get_table(self):
+        try:
+            table = self.soup.find("table", {"id" : "selectIndexForm:selectTable"})
+            table = table.find("tbody")
+            return table
+        except:
+            pass
+        
+    
+    def get_tests(self):
+        table = self.get_table()
+        try:
+            rows        = table.findAll("tr")
+            name        = table.findAll("span", {"class":"spanValue"})
+            time_limit  = table.findAll("span" , {"class":"currentSort"})
+            # due_date    = table.findAll("td" , {"class":"sorting_1"}) #TODO: get due date
+            for i in range(len(name)):
+                self.tests_list.append(f"{name[i].string}    |    {time_limit[i].string}      | ------to get the Due date")
+                i += 1
+        except:
+            pass
+       
