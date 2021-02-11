082ad7aa54492c09a245f1efcff7f24a06508ad8
Brandon Pahla <brandon.m.paahla@gmail.com>
2021-02-11 10:26:07 +0200

    Wrote an example of how to use Main.py

--- src/main/java/com/dash/dashboard/scraper/python/Main.py
+++ src/main/java/com/dash/dashboard/scraper/python/Main.py
@@ -7,7 +7,7 @@ from tools import Announcements, Tests
 
 
 # Loging in
-s = Scrape("student number", "password")
+s = Scrape("Student number", "password")
 
 Vula = Site(s)
 
@@ -21,9 +21,16 @@ sites_with_announcements = Vula.with_announcements() # [Key: site_name , Value: 
 sites_with_assignments = Vula.with_assignments() # [Key: site_name , Value: SiteTool object]
 
 # Getting all sites with Tests
-sites_withs_tests = Vula.with_tests() # [Key: site_name , Value: SiteTool object]
+sites_with_tests = Vula.with_tests() # [Key: site_name , Value: SiteTool object]
 
 # Getting all sites with Gradebook
 sites_with_gradebook = Vula.with_gradebook() # [Key: site_name , Value: SiteTool object]
 
- 
+# Getting all assignments in all sites with assignments
+# E.g
+for site_name in sites_with_assignments:
+    print("Printing assignments from ", site_name, " Site.")
+    assignments = sites_with_assignments[site_name].get_assignments()
+    for assignment in assignments:
+        print(assignment)
+    print()
