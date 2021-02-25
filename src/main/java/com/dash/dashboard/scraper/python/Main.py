13b93c200713af71c0db57316e8e61e1ee33d1f8
Brandon Pahla <69659996+Brandon-Pahla@users.noreply.github.com>
2021-02-25 12:50:29 +0200

    Update Main.py

--- src/main/java/com/dash/dashboard/scraper/python/Main.py
+++ src/main/java/com/dash/dashboard/scraper/python/Main.py
@@ -23,49 +23,49 @@ def main():
 
     # config = Configuration(Vula)
     
-    # if input("Refresh all? [y/n]: ").lower() == "y":
-    #     print("Fetching your data from Vula....")
-    #     config.updateAnnouncementsTable()
-    #     config.updateAssignmentsTable()
-    #     config.updateTestsTable()
-    #     config.updateGradebook()
+    if input("Refresh all? [y/n]: ").lower() == "y":
+        print("Fetching your data from Vula....")
+        config.updateAnnouncementsTable()
+        config.updateAssignmentsTable()
+        config.updateTestsTable()
+        config.updateGradebook()
 
-    # elif input("Refresh Announcements? [y/n]: ").lower() == "y":
-    #     print("Fetching your announcements from Vula....")
-    #     config.updateAnnouncementsTable()
+    elif input("Refresh Announcements? [y/n]: ").lower() == "y":
+        print("Fetching your announcements from Vula....")
+        config.updateAnnouncementsTable()
 
-    # elif input("Refresh Assignments? [y/n]: ").lower() == "y":
-    #     print("Fetching your assignments on Vula....")
-    #     config.updateAssignmentsTable()
+    elif input("Refresh Assignments? [y/n]: ").lower() == "y":
+        print("Fetching your assignments on Vula....")
+        config.updateAssignmentsTable()
 
-    # elif input("Refresh Tests? [y/n]: ").lower() == "y":
-    #     print("Fetching your tests from Vula....")
-    #     config.updateTestsTable()
+    elif input("Refresh Tests? [y/n]: ").lower() == "y":
+        print("Fetching your tests from Vula....")
+        config.updateTestsTable()
 
-    # elif input("Refresh Gradebook? [y/n]: ").lower() == "y":
-    #     print("Fetching your gradebook from Vula....")
-    #     config.updateGradebook()
+    elif input("Refresh Gradebook? [y/n]: ").lower() == "y":
+        print("Fetching your gradebook from Vula....")
+        config.updateGradebook()
     
 
-    # print(config.updateSitesTables())
+    print(config.updateSitesTables())
 
-    # if input("Print Announcements? [y/n]: ").lower() == "y":
-    #     print("Printing Announcements...")
-    #     config.printAnnouncements()
-    #     print()
-    # if input("Print Assignments [y/n]").lower() == "y":
-    #     print("Printing Assignments...")
-    #     config.printAssignments()
-    #     print()
+    if input("Print Announcements? [y/n]: ").lower() == "y":
+        print("Printing Announcements...")
+        config.printAnnouncements()
+        print()
+    if input("Print Assignments [y/n]").lower() == "y":
+        print("Printing Assignments...")
+        config.printAssignments()
+        print()
 
-    # if input("Print tests? [y/n]").lower() == "y":
-    #     print("Printing tests...")
-    #     config.printTests()
-    #     print()
-    # if input("Print gradebook? [y/n]").lower() == "y":
-    #     print("Printing gradebook...")
-    #     config.printGradebook()
-    #     print()
+    if input("Print tests? [y/n]").lower() == "y":
+        print("Printing tests...")
+        config.printTests()
+        print()
+    if input("Print gradebook? [y/n]").lower() == "y":
+        print("Printing gradebook...")
+        config.printGradebook()
+        print()
 
 if __name__ == "__main__":
     main()
